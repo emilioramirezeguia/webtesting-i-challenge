1eda3843db87ac7b5d500b1013028e220b059d02
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-19 18:24:26 -0700

    Test repair(item) method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("repair(item)", () => {
+  it("should restore an item's durability to 100", () => {
+    // Arrange
+    const originalItem = {};
+
+    const repairedItem = {
+      ...originalItem,
+      durability: 100,
+    };
+
+    // Act
+    const result = enhancer.repair(originalItem);
+
+    // Assert
+    expect(result).toEqual(repairedItem);
+    expect(originalItem).not.toEqual(repairedItem);
+  });
+});
