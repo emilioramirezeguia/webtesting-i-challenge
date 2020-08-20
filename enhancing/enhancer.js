aa8cd740b1c373edada04159807f9a132a95760c
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-19 20:39:48 -0700

    Test fail(item) method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -20,7 +20,21 @@ function success(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  const failedItem = {
+    ...item,
+  };
+
+  if (item.enhancement < 15 && item.durability >= 5) {
+    failedItem.durability -= 5;
+  } else if (item.enhancement >= 15 && item.durability >= 10) {
+    failedItem.durability -= 10;
+  } else if (item.enhancement > 16 && item.enhancement <= 20) {
+    failedItem.enhancement--;
+  } else {
+    console.log("This item is not valid.");
+  }
+
+  return failedItem;
 }
 
 function repair(item) {
